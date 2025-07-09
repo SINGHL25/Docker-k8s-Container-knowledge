3b7fe642446e43b2d8d736f28616a40d3c72d120
Akhilesh <akhi.singh1989@gmail.com>
2025-07-09 19:44:45 +1000

    Update app.py

--- 02_Docker_Compose/flask-redis-docker-compose/app.py
+++ 02_Docker_Compose/flask-redis-docker-compose/app.py
@@ -1,1 +1,14 @@
+from flask import Flask
+import redis
+
+app = Flask(__name__)
+r = redis.Redis(host='redis', port=6379)
+
+@app.route('/')
+def home():
+    count = r.incr('visits')
+    return f"Visit count: {count}"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
 
